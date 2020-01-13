98cbf5f3d3cbfe0d11d7e89ce7ecdc4a099c0304
KH036s <KH036s@KH036-18.uncanet.unca.edu>
2020-01-13 13:41:53 -0500

    lab1

---
+++ lab1/src/lab1/Lab1.java
@@ -0,0 +1,52 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Lab1 {
+	
+	public static double average(int[] arr) {
+		
+		double sum = 0;	
+		for(int i=0; i<arr.length; i++) {
+			sum+=arr[i];
+		}
+		
+		return sum/arr.length;
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello world.");
+		Scanner scan = new Scanner(System.in);
+//		System.out.println("enter something: ");
+//		String val = scan.nextLine();
+//		System.out.println("you entered " + val);
+//		scan.close();
+
+		
+		int elements = 3;
+		int[] input = new int[elements]; 
+		for(int i=0; i<elements; i++) {
+			
+			System.out.print("Enter an Integer: ");
+			String val = scan.nextLine();
+//			System.out.println("You entered " + val);
+			input[i] = Integer.parseInt(val);
+			
+		}
+		
+		System.out.println("inputs: ");
+		for(int i=0; i<elements; i++) {
+			System.out.println("input["+i+"] = "+input[i]);
+		}
+		
+		
+		double average = average(input);
+		System.out.println("The average is "+ average);
+		
+		
+	}
+
+	
+
+}
